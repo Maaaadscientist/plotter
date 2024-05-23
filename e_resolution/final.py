6a9db2aa312bc2bd338f44bdc25e230880d7e831
Mad Scientist <cobby319@gmail.com>
2024-05-23 11:33:40 +0800

    update the e_res script

--- e_resolution/final.py
+++ e_resolution/final.py
@@ -1,6 +1,7 @@
 import os
 import ROOT
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 import numpy as np
 from scipy.special import factorial
 import scienceplots
@@ -42,7 +43,7 @@ def borel_variance(lambda_):
 
 def calculate_resolution_single(overvoltage, P_ct, N):
     """Calculate the resolution for N Borel events for a single overvoltage point."""
-    lambda_ = np.log(1 / (1 - P_ct))
+    lambda_ = P_ct
     EX = borel_expected_value(lambda_)
     DX = borel_variance(lambda_)
     mean = N * EX
@@ -51,52 +52,57 @@ def calculate_resolution_single(overvoltage, P_ct, N):
     return resolution
 
 data_str = """
-2.5 0.08956
-2.6 0.09249
-2.7 0.09582
-2.8 0.09954
-2.9 0.10365
-3.0 0.10819
-3.1 0.11314
-3.2 0.11843
-3.3 0.12397
-3.4 0.12970
-3.5 0.13556
-3.6 0.14149
-3.7 0.14740
-3.8 0.15326
-3.9 0.15904
-4.0 0.16471
-4.1 0.17025
-4.2 0.17566
-4.3 0.18093
-4.4 0.18607
-4.5 0.19108
-4.6 0.19599
-4.7 0.20082
-4.8 0.20559
-4.9 0.21032
-5.0 0.21505
-5.1 0.21979
-5.2 0.22457
-5.3 0.22941
-5.4 0.23431
-5.5 0.23928
-5.6 0.24432
-5.7 0.24943
-5.8 0.25460
-5.9 0.25982
-6.0 0.26508
-6.1 0.27038
-6.2 0.27569
-6.3 0.28099
-6.4 0.28628
-6.5 0.29150
-6.6 0.29661
-6.7 0.30160
-6.8 0.30640
-6.9 0.31099
-7.0 0.31528
+2.0 0.039554078512139566
+2.1 0.04673173520968527
+2.2 0.05395834165366445
+2.3 0.0611255315829699
+2.4 0.0682123508213617
+2.5 0.07520389535428411
+2.6 0.08207868085239542
+2.7 0.08882076066269604
+2.8 0.09547469657732939
+2.9 0.1020550638045559
+3.0 0.10859526379831247
+3.1 0.11487868553242582
+3.2 0.1210656672178687
+3.3 0.12718387881813545
+3.4 0.13328220804343685
+3.5 0.1393866213222742
+3.6 0.1454972902561609
+3.7 0.1516187373248928
+3.8 0.1577786917724867
+3.9 0.16399653364949382
+4.0 0.17027976350191773
+4.1 0.1766385510592487
+4.2 0.18307513338117168
+4.3 0.1895814405166774
+4.4 0.19614365386415292
+4.5 0.20275770334778986
+4.6 0.20941796979813393
+4.7 0.21611790271754497
+4.8 0.22285156937380954
+4.9 0.22961924900939165
+5.0 0.23642615207014192
+5.1 0.24327887585410896
+5.2 0.25018357929549895
+5.3 0.25713830301706375
+5.4 0.2641376509313251
+5.5 0.27118011918504686
+5.6 0.2782610480172218
+5.7 0.28537978534211095
+5.8 0.29253622195875045
+5.9 0.2997364637608829
+6.0 0.30698778041246655
+6.1 0.3142996708371054
+6.2 0.3216761551901653
+6.3 0.32911516645346206
+6.4 0.3365876758915423
+6.5 0.3440678777573269
+6.6 0.3515313172755905
+6.7 0.35896559857726706
+6.8 0.36634660229010235
+6.9 0.3736504664441055
+7.0 0.38081627094317994
 """
 
 overvoltage_ct_list = extract_overvoltage_and_pct(data_str)
@@ -141,7 +147,7 @@ for s_file in special_files:
     full_path = os.path.join(directory_path, s_file)
     f = ROOT.TFile(full_path, "READ")
 
-    resolutions = calculate_energy_resolution(f.Get("hist_PDE"), f.Get("hist_dcr"), f.Get("hist_charge"))
+    resolutions = calculate_energy_resolution(f.Get("hist_PDE"), f.Get("hist_dcr"), f.Get("hist_qres"))
     key = 'max' if 'max' in s_file else 'typical'
     special_resolutions[key] = {'ct': resolutions[0], 'charge': resolutions[1]}
 
@@ -163,14 +169,6 @@ energy_resolutions_pde_analytical = []
 energy_resolutions_pte = []
 energy_resolutions_LS = []
 energy_resolutions_ct_dec = []
-energy_resolutions_gain_nuf3_uni = []
-energy_resolutions_gain_nuf5_uni = []
-energy_resolutions_gain_nuf10_uni = []
-energy_resolutions_gain_nuf15_uni = []
-energy_resolutions_gain_nuf3_gas = []
-energy_resolutions_gain_nuf5_gas = []
-energy_resolutions_gain_nuf10_gas = []
-energy_resolutions_gain_nuf15_gas = []
 energy_resolutions_final = []
 
 N_pe = []
@@ -193,7 +191,7 @@ for i,file in enumerate(root_files):
     hist_ap = f.Get("hist_ap")
     mean_ap, sigma_ap = fit_histogram(hist_ap)
 
-    hist_charge = f.Get("hist_charge")
+    hist_charge = f.Get("hist_qres")
     mean_charge, sigma_charge = fit_histogram(hist_charge)
     hist_dcr = f.Get("hist_dcr")
     mean_dcr, sigma_dcr = fit_histogram(hist_dcr)
@@ -205,62 +203,32 @@ for i,file in enumerate(root_files):
     mean_pde, sigma_pde = fit_histogram(hist_pde)
     hist_ls = f.Get("hist_LS")
     mean_ls, sigma_ls = fit_histogram(hist_ls)
-    hist_gain_nuf3_uni = f.Get("hist_charge_nuf3_uni")
-    mean_gain_nuf3_uni, sigma_gain_nuf3_uni = fit_histogram(hist_gain_nuf3_uni)
-    hist_gain_nuf5_uni = f.Get("hist_charge_nuf5_uni")
-    mean_gain_nuf5_uni, sigma_gain_nuf5_uni = fit_histogram(hist_gain_nuf5_uni)
-    hist_gain_nuf10_uni = f.Get("hist_charge_nuf10_uni")
-    mean_gain_nuf10_uni, sigma_gain_nuf10_uni = fit_histogram(hist_gain_nuf10_uni)
-    hist_gain_nuf15_uni = f.Get("hist_charge_nuf15_uni")
-    mean_gain_nuf15_uni, sigma_gain_nuf15_uni = fit_histogram(hist_gain_nuf15_uni)
-    hist_gain_nuf3_gas = f.Get("hist_charge_nuf3_gas")
-    mean_gain_nuf3_gas, sigma_gain_nuf3_gas = fit_histogram(hist_gain_nuf3_gas)
-    hist_gain_nuf5_gas = f.Get("hist_charge_nuf5_gas")
-    mean_gain_nuf5_gas, sigma_gain_nuf5_gas = fit_histogram(hist_gain_nuf5_gas)
-    hist_gain_nuf10_gas = f.Get("hist_charge_nuf10_gas")
-    mean_gain_nuf10_gas, sigma_gain_nuf10_gas = fit_histogram(hist_gain_nuf10_gas)
-    hist_gain_nuf15_gas = f.Get("hist_charge_nuf15_gas")
-    mean_gain_nuf15_gas, sigma_gain_nuf15_gas = fit_histogram(hist_gain_nuf15_gas)
+    hist_init = f.Get("hist_init")
+    mean_init, sigma_init = fit_histogram(hist_init)
 
+    print(file,sigma_ap, mean_ap) 
 
     over_voltage = extract_over_voltage(file)
     pct = 0
     for i in range(len(overvoltage_ct_list)):
         if round(overvoltage_ct_list[i][0],1) == round(over_voltage,1):
-            pct = overvoltage_ct_list[i][1]
-    energy_resolution_ct_analytical = calculate_resolution_single(over_voltage, pct, mean_pde)
+            pct = overvoltage_ct_list[i][1] * 2.05* 0.75
+    energy_resolution_ct_analytical = calculate_resolution_single(over_voltage, pct, mean_init)
     N_pe.append(mean_pde)
     print(over_voltage, energy_resolution_ct_analytical)
-    energy_resolution_ls = sigma_ls / mean_ls
-    energy_resolution_pte = sigma_pte / mean_pte
-    energy_resolution_pde = sigma_pde / mean_pde
-    energy_resolution_pde_sqrtN = np.sqrt(mean_pde) / mean_pde
-    energy_resolution_pde_analytical = np.sqrt(mean_pde * (1 - mean_pde/ mean_pte)) / mean_pde
-    energy_resolution_ct = sigma_ct / mean_ct
-    energy_resolution_ct_dec = np.sqrt(sigma_ct**2 - sigma_pde**2) / mean_ct
-    energy_resolution_ap = sigma_ap / mean_ap
-    energy_resolution_dcr = sigma_dcr / mean_dcr
+    energy_resolution_ls = sigma_ls / mean_init
+    energy_resolution_pte = sigma_pte / mean_init
+    energy_resolution_pde = sigma_pde / mean_init
+    energy_resolution_pde_sqrtN = np.sqrt(mean_pde) / mean_init
+    energy_resolution_pde_analytical = np.sqrt(mean_pde * (1 - mean_pde/ mean_pte)) / mean_init
+    energy_resolution_ct = sigma_ct / mean_init
+    energy_resolution_ct_dec = np.sqrt(sigma_ct**2 - sigma_dcr**2) / mean_init
+    energy_resolution_ap = sigma_ap / mean_init
+    energy_resolution_dcr = sigma_dcr / mean_init
     # Process "hist_dcr" and "hist_charge" using the correction factor for their means
 
-    energy_resolution_charge = sigma_charge  / mean_charge
-    energy_resolution_gain_nuf3_uni = np.sqrt(sigma_gain_nuf3_uni**2 - sigma_charge**2)  / mean_gain_nuf3_uni
-    energy_resolution_gain_nuf5_uni = np.sqrt(sigma_gain_nuf5_uni**2 - sigma_charge**2)  / mean_gain_nuf5_uni
-    energy_resolution_gain_nuf10_uni = np.sqrt(sigma_gain_nuf10_uni**2 - sigma_charge**2)  / mean_gain_nuf10_uni
-    energy_resolution_gain_nuf15_uni = np.sqrt(sigma_gain_nuf15_uni**2 - sigma_charge**2)  / mean_gain_nuf15_uni
-    energy_resolution_gain_nuf3_gas = np.sqrt(sigma_gain_nuf3_gas**2 - sigma_charge**2)  / mean_gain_nuf3_gas
-    energy_resolution_gain_nuf5_gas = np.sqrt(sigma_gain_nuf5_gas**2 - sigma_charge**2)  / mean_gain_nuf5_gas
-    energy_resolution_gain_nuf10_gas = np.sqrt(sigma_gain_nuf10_gas**2 - sigma_charge**2)  / mean_gain_nuf10_gas
-    energy_resolution_gain_nuf15_gas = np.sqrt(sigma_gain_nuf15_gas**2 - sigma_charge**2)  / mean_gain_nuf15_gas
-    energy_resolution_final = sigma_gain_nuf15_gas  / mean_gain_nuf15_gas
-    print(energy_resolution_gain_nuf3_uni)
-    energy_resolutions_gain_nuf3_uni.append(energy_resolution_gain_nuf3_uni)
-    energy_resolutions_gain_nuf5_uni.append(energy_resolution_gain_nuf5_uni)
-    energy_resolutions_gain_nuf10_uni.append(energy_resolution_gain_nuf10_uni)
-    energy_resolutions_gain_nuf15_uni.append(energy_resolution_gain_nuf15_uni)
-    energy_resolutions_gain_nuf3_gas.append(energy_resolution_gain_nuf3_gas)
-    energy_resolutions_gain_nuf5_gas.append(energy_resolution_gain_nuf5_gas)
-    energy_resolutions_gain_nuf10_gas.append(energy_resolution_gain_nuf10_gas)
-    energy_resolutions_gain_nuf15_gas.append(energy_resolution_gain_nuf15_gas)
+    energy_resolution_charge = sigma_charge / mean_init
+    energy_resolution_final = sigma_charge  / mean_init
     #energy_resolution_charge = sigma_charge  / mean_charge 
 
     energy_resolutions_final.append(energy_resolution_final)
@@ -292,19 +260,29 @@ plt.scatter(over_voltages, energy_resolutions_pte, label='LS+PTE',s=size_marker,
 plt.scatter(over_voltages, energy_resolutions_pde, label='LS+PTE+PDE',s=size_marker, marker='o', color='firebrick')
 plt.scatter(over_voltages, energy_resolutions_dcr, label='LS+PTE+PDE+DCR',s=size_marker, marker='o', color='mediumblue')
 plt.scatter(over_voltages, energy_resolutions_ct, label='LS+PTE+PDE+DCR+CT',s=size_marker, marker='o',color='orange')
-plt.scatter(over_voltages, energy_resolutions_ap, label='LS+PTE+PDE+DCR+CT+AP',s=size_marker, marker='o',color='lawngreen')
+#plt.scatter(over_voltages, energy_resolutions_ap, label='LS+PTE+PDE+DCR+CT+AP',s=size_marker, marker='o',color='lawngreen')
+plt.scatter(over_voltages, energy_resolutions_ct_analytical, label='Borel',s=size_marker, marker='o',color='lawngreen')
+plt.scatter(over_voltages, energy_resolutions_ct_dec, label='CT',s=size_marker, marker='o',color='red')
+
 plt.scatter(over_voltages, energy_resolutions_charge, label='LS+PTE+PDE+DCR+CT+AP+Q_RES',s=size_marker, marker='o', color='dimgrey')
-plt.scatter(over_voltages, energy_resolutions_final, label='LS+PTE+PDE+DCR+CT+AP+Q_RES+NUF0.3V',s=size_marker, marker='*', color='gold')
 #plt.scatter(over_voltages, energy_resolutions_dcr, label='DCR',s=size_marker)
 plt.axhline(y=special_resolutions['max']['charge'], color='r', linestyle='--', label='Max', linewidth=2)
 
 plt.axhline(y=special_resolutions['typical']['charge'], color='darkviolet', linestyle='--', label='Typical', linewidth=2)
 plt.ylim(0.005,0.024)
+# Function to format y-tick labels
+def to_percent(y, position):
+    return f'{y * 100:.2f}\%'
+
+# Applying the formatter to y-ticks
+plt.gca().yaxis.set_major_formatter(FuncFormatter(to_percent))
+
+# Setting font size for x and y ticks
 plt.xticks(fontsize=labelsize)
 plt.yticks(fontsize=labelsize)
 plt.xlabel("Over Voltage (V)", fontsize=labelsize, labelpad=15)
 plt.ylabel("Energy Resolution ($\\sigma \\mathrm{E} / \\mathrm{E}$)", fontsize=labelsize, labelpad=15)
-plt.title("Energy Resolution vs. Over Voltage", fontsize=titlesize, pad=20)
+#plt.title("Energy Resolution vs. Over Voltage", fontsize=titlesize, pad=20)
 plt.legend(loc='upper right',ncol=2,fontsize=textsize )
 plt.grid(True)
 plt.savefig("energy_res_final.pdf")
